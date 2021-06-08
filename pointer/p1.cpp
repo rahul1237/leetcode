1b7d6807d2e0123ff8c0144dca31cd15afad0136
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-06-08 17:37:50 +0530

    added

--- pointer/p1.cpp
+++ pointer/p1.cpp
@@ -4,16 +4,42 @@ using namespace std;
 int main(){
     
     int a=10;
-    int *aptr;
 
+    // created a pointer variable!
+    int* aptr;
+    // this stores the address of 'a'
     aptr=&a;
 
+    // returns the address of 'a'
     cout<<aptr<<endl;
+    // return the value of 'a'
     cout<<a<<endl;
+    // returns itself address!
     cout<<&a<<endl;
+    // returns itself address!
     cout<<&aptr<<endl;
-    cout<<*aptr;
+    // returns the value which is present at that point!
+    cout<<*aptr<<endl;
 
+    // updates the value which is present at that memory location!
+    *aptr=20;
+    cout<<a<<endl;
+    cout<<*aptr<<endl;
+
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+
+    // incrementing and decrementing the address of a which is present in 'aptr'
+    cout<<aptr<<endl;   
+    aptr++;
+    cout<<aptr<<endl;   
+
+    cout<<aptr<<endl;   
+    aptr--;
+    cout<<aptr<<endl;   
 
     return 0;
 }
