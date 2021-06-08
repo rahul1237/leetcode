611863b0efa8db6481f4b520c799bec6ee71f6ec
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-06-09 00:09:28 +0530

    added

---
+++ pointer/p2.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int arr[]={10,20,30};
+
+    // cout<<arr<<endl;
+    // cout<<*arr<<endl;
+
+    int* ptr=arr;
+
+    for(int i=0;i<3;i++){
+        cout<<*ptr<<endl;
+        ptr++;
+    };
+
+    cout<<endl;
+
+    for(int i=0;i<3;i++){
+        cout<<*(arr+i)<<endl;
+    };
+
+    return 0;
+}
